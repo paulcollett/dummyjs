64339b41718ba21add097e0237355122c41cb154
Paul Collett <paul@pcollett.com>
2017-11-24 15:01:55 -0800

    initial different dictionaries

---
+++ dictionaries/cupcake.js
@@ -0,0 +1,4 @@
+Dummy.addDictionary('cupcake', {
+  words: 'lemon drops lollipop muffin bar soufflé bear claw cheesecake pastry chupa chups beans carrot cupcake donut fruitcake gingerbread topping jujubes marzipan jelly sugar plum dessert caramels toffee icing pudding wafer canes danish cookie ice cream liquorice bonbon sesame snaps brownie oat cake biscuit cotton chocolate sweet tiramisu candy dragée tart apple pie tootsie roll halvah jelly-o macaroon croissant powder gummi bears gummies marshmallow',
+  joiners: 'I yo in hi at to'
+});
---
+++ dictionaries/hipster.js
@@ -0,0 +1,4 @@
+Dummy.addDictionary('hipster', {
+  words: '8-bit ethical reprehenderit non latte fixie mollit authentic 1982 moon helvetica dreamcatcher vinyl nulla carles bushwick bronson clothesline fin frado jug kale organic local fresh tassel liberal the of bennie chowder daisy gluten hog capitalism is vegan ut farm-to-table etsy incididunt sunt twee yr before gentrify whatever wes anderson chillwave dubstep sriracha voluptate pour-over esse trust-fund pinterest instagram dslr vintage dumpster totally selvage gluten-free brooklyn placeat delectus sint magna brony pony beer shot narwhal salvia letterpress art party street-art seitan anime wayfarers non-ethical viral iphone anim polaroid gastropub city classy original brew',
+  joiners: 'i of is ut yr'
+});
